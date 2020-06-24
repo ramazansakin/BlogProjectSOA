306cc7e132aff04f9e40372e4b14affea82f897c
r84160880 <ramazan.sakin@huawei.com>
2020-06-24 14:40:11 +0300

    Endpoints imrovements

--- src/main/java/com/sakinramzan/blogapp/controller/BlogController.java
+++ src/main/java/com/sakinramzan/blogapp/controller/BlogController.java
@@ -17,6 +17,7 @@ import java.util.List;
 import java.util.Optional;
 
 @RestController
+@RequestMapping("/blog-api")
 public class BlogController {
 
     @Autowired
--- src/main/java/com/sakinramzan/blogapp/controller/MainController.java
+++ src/main/java/com/sakinramzan/blogapp/controller/MainController.java
@@ -1,28 +1,30 @@
 package com.sakinramzan.blogapp.controller;
 
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
-@Controller
+@RestController
+@RequestMapping("/main")
 public class MainController {
 
-    @GetMapping(value="/")
-    public String index(){
+    @GetMapping(value = "/")
+    public String index() {
         return "index";
     }
 
-    @GetMapping(value="/registration")
-    public String register(){
+    @GetMapping(value = "/registration")
+    public String register() {
         return "register";
     }
 
-    @GetMapping(value="/login")
-    public String login(){
+    @GetMapping(value = "/login")
+    public String login() {
         return "login";
     }
 
-    @GetMapping(value="post/{id}")
-    public String singlePost(){
+    @GetMapping(value = "post/{id}")
+    public String singlePost() {
         return "post";
     }
 
