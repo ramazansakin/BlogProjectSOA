cd5601e04edb538c613e3dc517ae143cacd5b66b
r84160880 <ramazan.sakin@huawei.com>
2020-06-24 14:42:09 +0300

    Formatting

--- src/main/resources/static/js/script.js
+++ src/main/resources/static/js/script.js
@@ -5,46 +5,47 @@ function getCookie(name) {
 }
 
 function set_cookie(name, value) {
-    document.cookie = name +'='+ value +'; Path=/;';
+    document.cookie = name + '=' + value + '; Path=/;';
 }
+
 function delete_cookie(name) {
-    document.cookie = name +'=; Path=/; Expires=Thu, 01 Jan 1970 00:00:01 GMT;';
+    document.cookie = name + '=; Path=/; Expires=Thu, 01 Jan 1970 00:00:01 GMT;';
 }
 
 window.Event = new Vue({
     data: {isLoggedIn: false}
 });
 
-Vue.component('login-component',{
+Vue.component('login-component', {
     template: '<ul class="navMenu" v-if="!isLoggedIn()" style="float:right"><li style="margin-right:5px"><a href="/login">Login</a></li><li><a href="/registration">Register</a></li></ul>' +
         '<p v-else id="loggedIn">{{logged_in_msg}} <span v-on:click="logOut" v-show="isLoggedIn()"><b color="blue">Logout</b></span></p>',
-    data: function(){
-        return {logged_in_msg : ""}
+    data: function () {
+        return {logged_in_msg: ""}
     },
-    mounted(){
-        if(getCookie("access_token")){
+    mounted() {
+        if (getCookie("access_token")) {
             axios.get("/getUsername?access_token=" + getCookie("access_token"))
-                .then(function(response){
+                .then(function (response) {
                     this.logged_in_msg = "Welcome back , " + response.data;
                     window.Event.isLoggedIn = true;
                     Event.$emit('logged-in');
                 }.bind(this))
-                .catch(function(error){
+                .catch(function (error) {
                     delete_cookie("access_token");
                     return error;
                 });
         }
     },
-    methods : {
-        logOut(){
-            axios.get("/logouts?access_token="+getCookie("access_token"))
-                .then(function(response){
+    methods: {
+        logOut() {
+            axios.get("/logouts?access_token=" + getCookie("access_token"))
+                .then(function (response) {
                     window.Event.isLoggedIn = false;
-                    this.logged_in_msg  = "Successfully logged out";
+                    this.logged_in_msg = "Successfully logged out";
                     delete_cookie("access_token")
                 }.bind(this))
         },
-        isLoggedIn(){
+        isLoggedIn() {
             return window.Event.isLoggedIn;
         }
     }
